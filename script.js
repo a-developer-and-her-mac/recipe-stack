42359d199618d590241bac8c3ea846b73635b7a2
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2021-05-27 14:33:04 -0700

    Feat: add recipe area and category

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const recipeTitle = document.getElementById('recipe-title');
-const recipeText = document.getElementById('recipe-text');
+const recipeTitle = document.getElementById("recipe-title"),
+  recipeText = document.getElementById("recipe-text"),
+  recipeCategory = document.getElementById("recipe-category"),
+  recipeArea = document.getElementById("recipe-area");
 
 function handleClick() {
   fetch("https://www.themealdb.com/api/json/v1/1/random.php")
@@ -7,6 +9,8 @@ function handleClick() {
     .then(response => {
       recipeTitle.innerText = response.meals[0].strMeal;
       recipeText.innerText = response.meals[0].strInstructions;
+      recipeCategory.innerText = response.meals[0].strCategory;
+      recipeArea.innerText = response.meals[0].strArea;
     })
     .catch(error => {
       console.error(error);
