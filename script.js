e58f0feb1295e6c37f095596828f456e2c08f657
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2021-06-03 11:54:14 -0700

    Feat/Refactor: add complete recipe and redo code to make it cleaner

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-const recipeTitle = document.getElementById("recipe-title"),
-  recipeText = document.getElementById("recipe-text"),
-  recipeCategory = document.getElementById("recipe-category"),
-  recipeArea = document.getElementById("recipe-area");
+const card = document.getElementById('card');
 
 function handleClick() {
-  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-    .then(response => response.json())
-    .then(response => {
-      recipeTitle.innerText = response.meals[0].strMeal;
-      recipeText.innerText = response.meals[0].strInstructions;
-      recipeCategory.innerText = response.meals[0].strCategory;
-      recipeArea.innerText = response.meals[0].strArea;
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(res => {
+      createRecipe(res.meals[0]);
     })
-    .catch(error => {
-      console.error(error);
+    .catch(e => {
+      console.warn(e);
     });
+
+  //Inspiration (and some code) from Florin Pop 
+  const createRecipe = meal => {
+    const ingredients = [];
+
+    for (let i = 1; i <= 20; i++) {
+      if (meal[`strIngredient${i}`]) {
+        ingredients.push(
+          `${meal[`strIngredient${i}`]} : ${meal[`strMeasure${i}`]}`
+        );
+      } else {
+        break;
+      }
+    }
+
+    const setInnerHTML = `
+    <h2>${meal.strMeal}</h2>
+    <p>------------------</p>
+    <div class="recipe-instructions">
+    ${meal.strCategory
+        ? `<p>Category: ${meal.strCategory}</p>`
+        : ''
+    }
+    ${meal.strArea ? `<p>Area: ${meal.strArea}</p>` : ''}
+    </div>
+    <p><strong>Ingredients:</strong></p>
+    <ul>
+      ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+    </ul>
+    </div>
+    <p>${meal.strInstructions}</p>`
+
+    card.innerHTML = setInnerHTML;
+  }
 }
